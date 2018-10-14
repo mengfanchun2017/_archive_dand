cf7e3952bfe0fb339ee37beb740d34c7be20abf7
laomeng <mengfanchun1981@163.com>
2018-10-14 13:46:16 +0800

    update

--- project2/p2option/temp2.py
+++ project2/p2option/temp2.py
@@ -1,13 +1,62 @@
-fruit = {"apple", "banana", "orange", "grapefruit"}  # define a set
+Beatles_Discography = {'Rubber Soul': 1965, 'Magical Mystery Tour': 1967, "Sgt. Pepper's Lonely Hearts Club Band": 1967, 'Revolver': 1966, 'The Beatles': 1968, 'With the Beatles': 1963, 'Beatles for Sale': 1964, 'Yellow Submarine': 1969, "A Hard Day's Night": 1964, 'Help': 1965, 'Let It Be': 1970, 'Abbey Road': 1969, 'Twist and Shout': 1964, 'Please Please Me': 1963}
 
-print("watermelon" in fruit)  # check for element
+# 虽然dic可以在函数中编辑外部变量
+# 但建议还是把只在函数内使用的变量放到函数内
+# dict_result = {}
+def most_prolific(dictary):
+    dict_result = {}
+    for year_item in dictary:
+        if dict_result.get(dictary[year_item]) != None:
+            dict_result[dictary[year_item]]+=1
+        else:
+            dict_result[dictary[year_item]]=1
+            
+    array_temp=[]
+    index_temp=0
+    for year in dict_result:
+        value_count = dict_result.get(year)
+        print('===',year,'+++',value_count)
+        if index_temp < value_count:
+            index_temp = value_count
+            array_temp = []
+            array_temp.append(str(year))
+        elif index_temp == value_count:
+            array_temp.append(str(year))
+        else:
+            continue
+        
+    if len(array_temp)==1:
+        return "".join(array_temp)
+    else:
+        return int(array_temp)
+    
+print("==+++",most_prolific(Beatles_Discography))
 
-fruit.add("watermelon")  # add an element
-print(fruit)
+def most_prolific(discs): 
+#We will store a dictionary of years 
+#and number of albums per year     
+    years = {} 
+    maxyears = [] 
+    maxnumber = 0 
+    for disc in discs: 
+        year = discs[disc]
+        if year in years: 
+            years[year] += 1 
+        else: 
+            years[year] = 1 
 
-print(fruit.pop())  # remove a random element
-print(fruit.pop())
-print(fruit.pop())
-print(fruit.pop())
-print(fruit)
-
+#find the year in which the maximum 
+#number of albums was published 
+#there are more elegant ways of accomplishing this, 
+#but the code below works 
+    for year in years:
+        if years[year] > maxnumber: 
+            maxyears=[] 
+            maxyears.append(year) 
+            maxnumber = years[year] 
+        elif years[year] == maxnumber and not (year in maxyears): 
+            maxyears.append(year) 
+    if (len(maxyears) == 1): 
+        return maxyears[0] 
+    else: 
+        return maxyears
---
+++ project2/p2option/temp4.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,5]
+
+print(list)
+
+for i in list:
+    print(i)
