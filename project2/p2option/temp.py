4bdeaabdf57a940691f8d18f5ba5e1471f8a36bb
laomeng <mengfanchun1981@163.com>
2018-11-10 23:35:47 +0800

    update

--- project2/p2option/temp.py
+++ project2/p2option/temp.py
@@ -1,15 +1,3 @@
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as pl
 
-df = pd.DataFrame({'A': 'foo bar foo bar foo bar foo foo'.split(),
-                   'B': 'one one two three two two one three'.split(),
-                   'C': np.arange(8), 'D': np.arange(8) * 2})
-print(df)
-
-query1 = df.query('A == "foo"')
-query2 = df.query('C > 1')
-query3 = df.query('C > 1 and D > 4')
-
-print(query1)
-print(query2)
-print(query3)
+pl.figure(figsize=(10,7))
